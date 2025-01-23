4c1640eaf406a1a439895c40be216b1fcfadc2c5
Matthew Doty <matt@w-d.org>
2025-01-23 09:23:20 -0500

    No longer using SpaceMono regular

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -12,19 +12,9 @@ SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
-
   useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
+    SplashScreen.hideAsync();
+  }, []);
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
